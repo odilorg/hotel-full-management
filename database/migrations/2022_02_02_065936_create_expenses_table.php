dc6d676431d23b1fa61db83fd7b57f441a9e06e9
Jon <odilorg@gmail.com>
2022-02-02 13:26:35 +0500

    started expense model

---
+++ database/migrations/2022_02_02_065936_create_expenses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExpensesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('expense_name');
+            $table->foreignId('user_id');
+            $table->date('expense_date');
+            $table->foreignId('expense_category_id');
+            $table->foreignId('payment_type_id');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expenses');
+    }
+}
