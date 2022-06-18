04c482fafbf97778cc80046aaaa71c20d351c072
Odiljon <odilorg@gmail.com>
2022-06-18 19:10:28 +0500

    now added new bot

--- app/Http/Controllers/CleaningController.php
+++ app/Http/Controllers/CleaningController.php
@@ -12,21 +12,21 @@ class CleaningController extends Controller
         return view('cleaning.cleaning');
     }
 
-    public function cleaning_notready (Request $request)
-    {
-        //php code to send the message to Telegram Channel
-        $xona = $request->input('xona');
-        $apiToken = "1750871148:AAE82UE_OMk_FskoJh61wfT6Oowf6i4RwHk";
-        $data = [
-            'chat_id' => '-1001570852064', 
-            'text' => $xona." Xona Tayor Emas",
-        ];
-        $response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );  
-        // return view('cleaning.cleaning');
-        session()->flash('success', 'Raxmat');
-        session()->flash('type', 'Yangi Cargo');
-        return back();
-    }
+    // public function cleaning_notready (Request $request)
+    // {
+    //     //php code to send the message to Telegram Channel
+    //     $xona = $request->input('xona');
+    //     $apiToken = "1750871148:AAE82UE_OMk_FskoJh61wfT6Oowf6i4RwHk";
+    //     $data = [
+    //         'chat_id' => '-1001570852064', 
+    //         'text' => $xona." Xona Tayor Emas",
+    //     ];
+    //     $response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );  
+    //     // return view('cleaning.cleaning');
+    //     session()->flash('success', 'Raxmat');
+    //     session()->flash('type', 'Yangi Cargo');
+    //     return back();
+    // }
     public function cleaning_ready ( Request $request)
     {
         
@@ -34,14 +34,14 @@ class CleaningController extends Controller
        // dd($xona);
         //php code to send the message to Telegram Channel
 
-        $apiToken = "1750871148:AAE82UE_OMk_FskoJh61wfT6Oowf6i4RwHk";
+        $apiToken = "5323496366:AAEi_xDnyz2uwL7YNS1p7ayDxz854LH-HTg";
         $data = [
-            'chat_id' => '-1001570852064', 
-            'text' =>  $xona ." Xona Tayor ",
+            'chat_id' => '-653810568', 
+            'text' =>  $xona ." Xona Tayor ". now(),
         ];
         $response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );  
         session()->flash('success', 'Raxmat');
-        session()->flash('type', 'Yangi Cargo');
+        session()->flash('type', ' Xona Xolati');
         return back();
     }
 }
--- resources/views/cleaning/cleaning.blade.php
+++ resources/views/cleaning/cleaning.blade.php
@@ -7,7 +7,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Companies</h1>
+                    <h1>Cleaning</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
