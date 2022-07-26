133c828f0436c64c8565fe07c22e0b6bfde529a3
Odiljon <odilorg@gmail.com>
2022-07-26 17:30:18 +0500

    fix lastnight

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -493,13 +493,13 @@ public function unpaid(Request $request) {
     //dd($nites);
     foreach ($nites as $nite) {
        // dd( $nite->numAdult);
-       if($nite->firstNight > '2022-05-06') {
-        $f_nite = new Carbon($nite->firstNight); 
+       if($nite->firstNight < '2022-05-06') {
+       // $f_nite = new Carbon($nite->firstNight); 
         $l_nite = new Carbon($nite->lastNight);
-       // $l_nite = $l_nite->addDays(1);
-        $diff_days = ($f_nite->diffInDays($l_nite) );
+        $l_nite = $l_nite->addDays(1);
+        //$diff_days = ($f_nite->diffInDays($l_nite) );
        // $nite = new Reservation ;
-        $nite->nites = $nite->numAdult *  $diff_days;
+        $nite->lastNight =  $l_nite;
         
         $nite->save();
        }
