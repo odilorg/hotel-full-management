e950f5c06a237bc824ec76d8abf537a3fcc0cff1
OJ <odilorg@gmail.com>
2022-12-26 17:29:33 +0500

    sdd

--- app/Http/Controllers/Beds24bookingController.php
+++ app/Http/Controllers/Beds24bookingController.php
@@ -89,7 +89,7 @@ class Beds24bookingController extends Controller
                 ['guestName' => $fullname,
                 'company_name' => $position_status,
                 'referer' => $referer,
-                'company_name' => $part_cut
+               
                 
                 ]
 
