f2be62ab976bc629a6bdad9870ef81d1df06296c
OJ <odilorg@gmail.com>
2022-12-26 17:21:02 +0500

    6

--- app/Http/Controllers/Beds24bookingController.php
+++ app/Http/Controllers/Beds24bookingController.php
@@ -71,8 +71,8 @@ class Beds24bookingController extends Controller
         $fullname = $request->header('fullname');
         $referer = $request->header('referer');
         $payment = htmlentities($request->header('payment'));
-        $position_status = strrpos($payment, "conf_status")+15;
-        $part_cut = substr($payment, $position_status, 23);
+        $position_status = strrpos($payment, "conf_status");
+        $part_cut = substr($payment, $position_status, 6);
         $ready_status = trim($part_cut,";&");
         $status = $request->status;
         $bookid = $request->bookid;
