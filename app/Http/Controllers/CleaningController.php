e8e9dc82977f864361c42ff542f79450f56f7c26
ODil <odilorg@gmail.com>
2023-01-01 22:34:18 +0500

    done roomready

--- app/Http/Controllers/CleaningController.php
+++ app/Http/Controllers/CleaningController.php
@@ -44,17 +44,19 @@ class CleaningController extends Controller
         $telegram_api = $_ENV['TELEGRAMAPI'];
         $room_id = $request->input('room_id');
 
-      //  dd($room_id);
+    //   /  dd($room_id);
         $propert_id = $request->input("property_id");
         // set room status to READY from posting to 
         // getiing room id and unit id
-        date_default_timezone_set("Asia/Tashkent");
+        //date_default_timezone_set("Asia/Tashkent");
          //$propert_id = Room::where('room_number', $room_id)->first();
-
-         $booking_id = Beds24booking::where('lastNight', date("Y-m-d"))
-         ->where('room_id', $room_id)
-         ->first();
-         $xona = $booking_id->room->room_number;
+$room = Room::find($room_id);
+//dd($room->room_name);
+        //  $booking_id = Beds24booking::where('lastNight', date("Y-m-d"))
+        //  ->where('room_id', $room_id)
+        //  ->first();
+       //  dd($booking_id);
+        $xona = $room->room_number;
 
         // dd($booking_id->bookId);
 //set beds24 booking room status
@@ -64,16 +66,30 @@ $bedsProkey = $_ENV['BEDS24PROPKEY2'];
 //dd(gettype($bedsapi));
 //dd($booking_id->bookId);
 
-$response = Http::post('https://api.beds24.com/json/setBooking', [
+$response = Http::post('https://api.beds24.com/json/setProperty', [
 
-"authentication" => [
+
+
+    "authentication" => [
     "apiKey" => $bedsapi,
     "propKey" => $bedsProkey
 ],
-"bookId" => $booking_id->bookId,
-"infoItems" => [
+
+"setProperty" => [
     [
-        "code" => "ROOMREADY"
+        "action" => "modify",
+        "roomTypes" => [
+            [
+                "action" => "modify",
+                "roomId" => $room->room_id,
+                "units" => [
+                    $room->unit_id => [
+                        "unitStatusColor" => "#1578db",
+                        "unitStatusText" => "clean"
+                    ]
+                ]
+            ]
+        ]
     
     ]
     
@@ -85,7 +101,7 @@ $response = Http::post('https://api.beds24.com/json/setBooking', [
 ]);
 
 
-     dd($response);   
+   //  dd($response);   
         
         // $response = Http::post('http://example.com/users', [
         //     'name' => 'Steve',
