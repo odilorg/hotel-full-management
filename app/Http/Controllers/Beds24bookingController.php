2d13c37ad4c9ef5bbef798f01c61fc58dc171772
OJ <odilorg@gmail.com>
2023-02-04 15:56:05 +0500

    s

--- app/Http/Controllers/Beds24bookingController.php
+++ app/Http/Controllers/Beds24bookingController.php
@@ -206,7 +206,7 @@ if (is_null($payment_method)) {
        $htmltext = $request->all();
         $hotel_id = $request->header('propertyid');
        $room_name = $request->header('room_name');
-      info('df', $htmltext);
+      Log::info($htmltext);
         if ($hotel_id == 41097) {
            
              $apiToken = $_ENV['TELEGRAMAPIJAHONGIR'];
