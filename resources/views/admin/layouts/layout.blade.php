48b1981201f8b29cfd9cbba4db2e74dbc870f61b
Odiljon <odilorg@gmail.com>
2023-12-10 18:21:43 +0500

    done company table mobile

--- resources/views/admin/layouts/layout.blade.php
+++ resources/views/admin/layouts/layout.blade.php
@@ -44,7 +44,85 @@ scratch. This page gets rid of all links and provides the needed markup only.
         }
     </script>
     <style>
-        
+   @media screen and (min-width: 761px) {
+    #customers {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+#customers td, #customers th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+#customers tr:nth-child(even){background-color: #f2f2f2;}
+#customers tr:hover {background-color: #ddd;}
+#customers th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #04AA6D;
+  color: white;
+}
+}        
+       
+/*
+	Max width before this PARTICULAR table gets nasty. This query will take effect for any screen smaller than 760px and also iPads specifically.
+	*/
+	@media
+	  only screen 
+    and (max-width: 760px) 
+     {
+		/* Force table to not be like tables anymore */
+		table, thead, tbody, th, td, tr {
+			display: block;
+		}
+		/* Hide table headers (but not display: none;, for accessibility) */
+		thead tr {
+			position: absolute;
+			top: -9999px;
+			left: -9999px;
+		}
+    tr {
+      margin: 0 0 1rem 0;
+    }
+      
+    tr:nth-child(odd) {
+      background: #ccc;
+    }
+    
+		td {
+			/* Behave  like a "row" */
+			border: none;
+			border-bottom: 1px solid #eee;
+			position: relative;
+			padding-left: 50%;
+		}
+		td:before {
+			/* Now like a table header */
+			position: absolute;
+			/* Top/left values mimic padding */
+			top: 0;
+			left: 6px;
+			width: 45%;
+			padding-right: 10px;
+			white-space: nowrap;
+		}
+		/*
+		Label the data
+    You could also use a data-* attribute and content for this. That way "bloats" the HTML, this way means you need to keep HTML and CSS in sync. Lea Verou has a clever way to handle with text-shadow.
+		*/
+		td:nth-of-type(1):before { content: "Company Name"; }
+		td:nth-of-type(2):before { content: "Company Official name"; }
+		td:nth-of-type(3):before { content: "INN"; }
+        td:nth-of-type(4):before { content: "Company Street"; }
+        td:nth-of-type(5):before { content: "Company Phone"; }
+        td:nth-of-type(6):before { content: "Company Email"; }
+        td:nth-of-type(7):before { content: "Company Acc #"; }
+        td:nth-of-type(8):before { content: "Company Bank"; }
+        td:nth-of-type(9):before { content: "Bank MFO"; }
+        td:nth-of-type(10):before { content: "Company Address City"; }
+		
+	}      
 
 .button {
   text-align: center;
